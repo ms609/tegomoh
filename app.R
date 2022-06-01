6f476b9cbfbd8f7b52ca44ed2c082f375d5f5d9b
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2022-06-01 15:22:05 +0100

    Simple palettes

--- app.R
+++ app.R
@@ -28,31 +28,6 @@ if (!requireNamespace("colourpicker", quietly = TRUE)) {
 }
 library("colourpicker")
 
-
-# Configure palettes and custom input selectors
-palettes <- list("#91aaa7",
-                 c("#969660", "#c3dfca"),
-                 c("#be83ae", "#2ea7af", "#fbcdcf"),
-                 c("#72c5a9", "#b7c5ff", "#dbdabb", "#a28bac"),
-                 c("#59c4c0", "#ea9a9a", "#7998a6", "#e9d7a9", "#9c9379"),
-                 c("#e8b889", "#67c6eb", "#e5d5c2", "#938fba", "#64b69a", "#779c7b"),
-                 c("#c4808f", "#5ba08f", "#f0a693", "#ccd7fe", "#cdb87e", "#c6aae2", "#d2dad8"),
-                 c("#d0847f", "#63a5d7", "#d7b981", "#5a9bbb", "#9bb67e", "#dea6d5", "#91967e", "#ca7f96"),
-                 c("#8b93a8", "#ccb97e", "#8e9dd7", "#57a384", "#dbb1e7", "#2da7af", "#d68986", "#75d2f9", "#e4d1f0"),
-                 c("#dfcf92", "#40b3cb", "#b88a61", "#ecb2e0", "#d6dbbc", "#a28bae", "#edcfeb", "#7498ab", "#b187a0", "#8f939c"),
-                 c("#a98f70", "#7be5e2", "#d295c0", "#9ae2bd", "#d3b7f1", "#eca88d", "#8993cd", "#ffc7bb", "#8695a8", "#b3e1df", "#b6878a"),
-                 c("#eaa9d3", "#7ac09b", "#fdaca8", "#8ce7e4", "#eed69b", "#70a4d9", "#e8d6ba", "#589bbb", "#959672", "#d0dbd1", "#9b9282", "#d9d9c6"),
-                 c("#7498ab", "#e5bd8a", "#7ed8ff", "#de8f8e", "#46bac6", "#ffc0d3", "#57ae96", "#f7cddd", "#50a098", "#b58a6d", "#add49d", "#a18da1", "#cedad9"),
-                 c("#8097a4", "#d0dea9", "#a78cc3", "#aee4bf", "#bb82a8", "#5dc9c6", "#b88690", "#26a3b9", "#ad8e6f", "#a4e2ef", "#869a65", "#efcfdd", "#60a089", "#9e927b"),
-                 c("#b9aae5", "#bbd69c", "#e2adde", "#77a777", "#f8abc8", "#8ee7ce", "#f2a1a5", "#81bdf1", "#f2bb91", "#b8dcfe", "#aeb276", "#f2cdef", "#e8d6b2", "#8d92b0", "#b7878d"),
-                 c("#c3d79b", "#b28cc0", "#64a985", "#e3a7d4", "#2ea2aa", "#e69892", "#85c6f9", "#fbd1a0", "#7696be", "#89996c", "#ddcdff", "#719d89", "#f5cde6", "#b6e0da", "#e8d4cd", "#b5ddfa"),
-                 c("#a98d83", "#84e1ff", "#bb8964", "#46b1d1", "#ffbfa5", "#6199c0", "#bbcb8f", "#bf82ab", "#85ddc4", "#eea0ba", "#c1d8ff", "#c3818b", "#c5c6ff", "#999388", "#e8cbff", "#ffb5b6", "#d2dad7"),
-                 c("#faccde", "#60a987", "#c6abe4", "#6f9e77", "#c48093", "#a5e5d3", "#cc8f6f", "#499fae", "#d9dca6", "#7796b8", "#bee1ba", "#b4daff", "#919583", "#e2d3e9", "#47a19b", "#ebd4bc", "#7c9993", "#a9e3e0"),
-                 c("#739e6e", "#ffbfd9", "#43b6bb", "#e8ad88", "#5e9bce", "#c2af75", "#a8e0fe", "#fad0a8", "#679e8d", "#ffc7b1", "#abe5c0", "#ac8d78", "#c5dddc", "#a48f84", "#cadfb0", "#899694", "#fdcdc1", "#d1dad5", "#dfd8c4"),
-                 c("#6e9c93", "#ffb4b3", "#7ec6a2", "#eeccfe", "#cddb9d", "#8a90c5", "#dcb983", "#77bff0", "#f0ab92", "#90ddff", "#f1d3a9", "#b5c2fe", "#c1e1b7", "#7596ba", "#bce1c4", "#a88c96", "#5a9daf", "#b18b80", "#d4d6f3", "#949577"),
-                 c("#e7d6bb", "#749ed5", "#f9d29d", "#67b3e2", "#d09773", "#65ccec", "#d38585", "#7fe8ef", "#cf8190", "#94e8cd", "#ae8cc1", "#b3cf95", "#cbc0fc", "#94a66c", "#eeccff", "#ada368", "#e9a6ce", "#48a297", "#ffc1df", "#799c7a", "#facbe0", "#5d9e9a", "#ffc6c1", "#619bb0", "#fccdcb", "#7197bb", "#b1e4c3", "#9390b1", "#c3e0c0", "#a98c90", "#ade3ce", "#9c927d", "#c2dafe", "#869881", "#e6d3dc", "#6e9ba4", "#bde0d0", "#8196a4", "#b2e1df", "#b9deea")
-)
-
 ltyInput <- function (id, name, val, none = TRUE) {
   selectInput(id, paste(name, "line type"),
               c(if (none) list("None" = "blank"),
@@ -108,8 +83,8 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      tabsetPanel(
-        tabPanel("Load data",
+      # tabsetPanel(
+        # tabPanel("Load data",
                  fileInput("treeFile", "Load tree",
                            placeholder = "No tree file selected"),
                  hidden(numericInput("whichTree", "Use tree number:",
@@ -122,64 +97,67 @@ ui <- fluidPage(
                  hidden(selectInput("ptCol", "Colour points by",
                                     list("Cluster" = "Cluster",
                                          "Fixed" = "Fixed"), "Cluster")),
-                 hidden(selectInput("pch", "oints by",
+                 hidden(selectInput("pch", "Style points by",
                                     list("Cluster" = "Cluster",
                                          "Fixed" = "Fixed"), "Fixed")),
-                 sliderInput("ptCex", "Point size", 0.5, 9.5, 2.5)
-        ),
-        tabPanel("Plot display",
-                 
-                 
-                 colourInput('col', 'Background colour', '#ffffff'),
-                 checkboxGroupInput('display', 'Display options', 
-                                    list('Clockwise' = 'clockwise',
-                                         'Isometric' = 'isometric',
-                                         'Tip labels' = 'show.tip.labels',
-                                         'Axis labels' = 'show.axis.labels',
-                                         'Axis tick labels' = 'axis.labels',
-                                         'Axis tick marks' = 'axis.tick',
-                                         'Rotate tick labels' = 'axis.rotate'), 
-                                    c('clockwise', 'isometric', 'axis.labels',
-                                      'show.axis.labels', 'axis.tick', 'axis.rotate')),
-                 lwdInput('axis.lwd', 'Axis', 1),
-                 ltyInput('axis.lty', 'Axis', 'solid'),
-                 colourInput('axis.col', 'Axis colour', "black"),
-                 lwdInput('ticks.lwd', 'Axis ticks', 1),
-                 sliderInput('ticks.length', 'Axis tick length', 0, 0.1, 0.025),
-                 colourInput('ticks.col', 'Axis tick colour', "darkgrey"),
-
-        ),
-
-        tabPanel('Points',
-                 selectInput('points.type', 'Plot type', 
-                             list('Points only' = 'p',
-                                  'Lines' = 'l',
-                                  'Connected points' = 'b',
-                                  'Text' = 'text'),
-                             'p'),
-                 selectInput('text.source', 'Text to display',
-                             list('Row names' = 0,
-                                  'Data column 4' = 4,
-                                  'Data column 5' = 5,
-                                  'Data column 6' = 6),
-                             0),
-                 pchInput('points.pch', 'Point shape', 16),
-                 selectInput('points.col.by', 'Point colour',
-                             list('Data column 4' = 4,
-                                  'Data column 5' = 5,
-                                  'Data column 6' = 6,
-                                  'User-specified' = 0),
-                             0),
-                 colourInput('points.col', 'Colour', '#222222'),
-                 cexInput('points.cex', 'Point size', 1.8),
-                 lwdInput('points.lwd', 'Connecting', 1),
-                 ltyInput('points.lty', 'Connecting', 'solid', FALSE),
-        )
-      ),
+                 sliderInput("ptCex", "Point size", 0.5, 9.5, 2.5),
+                 checkboxGroupInput("Display", "Display:", 
+                                    list("Cluster boundaries" = "hulls"),
+                                    c("hulls"))
+        # ),
+        # tabPanel("Plot display",
+        #          
+        #          
+        #          colourInput('col', 'Background colour', '#ffffff'),
+        #          checkboxGroupInput('display', 'Display options', 
+        #                             list('Clockwise' = 'clockwise',
+        #                                  'Isometric' = 'isometric',
+        #                                  'Tip labels' = 'show.tip.labels',
+        #                                  'Axis labels' = 'show.axis.labels',
+        #                                  'Axis tick labels' = 'axis.labels',
+        #                                  'Axis tick marks' = 'axis.tick',
+        #                                  'Rotate tick labels' = 'axis.rotate'), 
+        #                             c('clockwise', 'isometric', 'axis.labels',
+        #                               'show.axis.labels', 'axis.tick', 'axis.rotate')),
+        #          lwdInput('axis.lwd', 'Axis', 1),
+        #          ltyInput('axis.lty', 'Axis', 'solid'),
+        #          colourInput('axis.col', 'Axis colour', "black"),
+        #          lwdInput('ticks.lwd', 'Axis ticks', 1),
+        #          sliderInput('ticks.length', 'Axis tick length', 0, 0.1, 0.025),
+        #          colourInput('ticks.col', 'Axis tick colour', "darkgrey"),
+        # 
+        # ),
+        # 
+        # tabPanel('Points',
+        #          selectInput('points.type', 'Plot type', 
+        #                      list('Points only' = 'p',
+        #                           'Lines' = 'l',
+        #                           'Connected points' = 'b',
+        #                           'Text' = 'text'),
+        #                      'p'),
+        #          selectInput('text.source', 'Text to display',
+        #                      list('Row names' = 0,
+        #                           'Data column 4' = 4,
+        #                           'Data column 5' = 5,
+        #                           'Data column 6' = 6),
+        #                      0),
+        #          pchInput('points.pch', 'Point shape', 16),
+        #          selectInput('points.col.by', 'Point colour',
+        #                      list('Data column 4' = 4,
+        #                           'Data column 5' = 5,
+        #                           'Data column 6' = 6,
+        #                           'User-specified' = 0),
+        #                      0),
+        #          colourInput('points.col', 'Colour', '#222222'),
+        #          cexInput('points.cex', 'Point size', 1.8),
+        #          lwdInput('points.lwd', 'Connecting', 1),
+        #          ltyInput('points.lty', 'Connecting', 'solid', FALSE),
+        # )
+      # ),
     ),
     
     mainPanel(
-      fluidRow(plotOutput(outputId = "mainPlot", width = "70%", height = "800px")),
+      fluidRow(plotOutput(outputId = "mainPlot", width = "80%", height = "900px")),
       fluidRow(textOutput(outputId = "plotQual")),
       fluidRow(id = "saveButtons",
                tags$span("Save as: "),
@@ -379,8 +357,7 @@ server <- function(input, output, session) {
                         choices = setNames(metaOpts, metaOpts),
                         selected = input$ptCol)
       updateSelectInput(session, "pch",
-                        choices = setNames(metaOpts, metaOpts), 
-                        selected = cn[1])
+                        choices = setNames(metaOpts, metaOpts))
     } else {
       dput(ret)
       hide("ptCol")
@@ -390,7 +367,6 @@ server <- function(input, output, session) {
     ret
   })
   
-  
   DoPlot <- function() {
     if (treeLoaded()) {
       
@@ -398,65 +374,79 @@ server <- function(input, output, session) {
       metadata()
       switch(input$ptCol,
              "Cluster" = {
-               colby <- cluster
+               colby <- hcl.colors(max(cluster), "dark2")[cluster]
              }, "Fixed" = {
                colby <- 1
              }, {
-               colCategories <- as.factor(metadata()[names(cluster), input$pch])
-               colby <- as.integer(colCategories)
+               colCategories <- as.factor(metadata()[names(cluster), input$ptCol])
+               dput(colCategories)
+               pal <- hcl.colors(length(levels(colCategories)), "dark2", 0.9)
+               colby <- pal[as.integer(colCategories)]
              }
       )
       switch(input$pch,
              "Cluster" = {
                pchby <- cluster
              }, "Fixed" = {
-               pchby <- 15
+               pchby <- 16 # filled circle
              }, {
                pchCategories <- as.factor(metadata()[names(cluster), input$pch])
                pchby <- as.integer(pchCategories)
              }
       )
       
-      par(mfrow = c(2, 1), mar = rep(0.1, 4))
+      layout(matrix(c(1, 1, 2, 3), 2, 2), widths = c(1, 2), heights = c(2, 1))
+      par(mar = rep(0.1, 4))
       plot(tree(), tip.color = colby, cex = 0.7)
       plot(mapping(), asp = 1, frame.plot = FALSE, axes = FALSE,
            xlab = "", ylab = "", cex = input$ptCex,
            col = colby, pch = pchby)
       
-      for (clI in unique(cluster)) {
-        inCluster <- cluster == clI
-        clusterX <- mapping()[inCluster, 1]
-        clusterY <- mapping()[inCluster, 2]
-        hull <- chull(clusterX, clusterY)
-        grown <- Ternary::GrowPolygon(clusterX[hull], clusterY[hull], 0.3)
-        polygon(grown, lty = 1, lwd = 2, border = clI)
-        text(mean(clusterX), mean(clusterY), clI, col = clI, font = 2, pos = 1)
+      # Hulls
+      if ("hulls" %in% input$display) {
+        for (clI in unique(cluster)) {
+          inCluster <- cluster == clI
+          clusterX <- mapping()[inCluster, 1]
+          clusterY <- mapping()[inCluster, 2]
+          hull <- chull(clusterX, clusterY)
+          grown <- Ternary::GrowPolygon(clusterX[hull], clusterY[hull], 0.3)
+          polygon(grown, lty = 1, lwd = 2, border = clI)
+          text(mean(clusterX), mean(clusterY), clI, col = clI, font = 2, pos = 1)
+        }
       }
       
+      # Scale bar
       corners <- par("usr")
       nSNP <- 5L
-      lines(c(corners[2] - 1, corners[2] - 1 - nSNP),
-            rep(corners[3], 2) + 1, lwd = 2)
-      if (FALSE) {
-        legend("topright", pch = 1,
-               col = seq_along(levels(categories)),
-               levels(categories))
-      }
       text(corners[2] - 1 - (nSNP / 2), corners[3] + 1,
-           paste0("~", nSNP, " SNP"), pos = 3)
+           paste0("~", nSNP, " SNP"), pos = 3, xpd = NA)
+      lines(c(corners[2] - 1, corners[2] - 1 - nSNP),
+            rep(corners[3], 2) + 1, lwd = 2, xpd = NA)
       
-      poEdge <- Postorder(tree())$edge
-      parent <- poEdge[, 1]
-      child <- poEdge[, 2]
-      xy <- matrix(0, parent[1], 2)
-      xy[seq_len(NTip(tree())), ] <- mapping()
-      for (node in unique(parent)) {
-        xy[node, ] <- colMeans(xy[child[parent == node], , drop = FALSE])
+      
+      plot.new()
+      if (!input$ptCol %in% c("Fixed", "Cluster")) {
+        legend("right", pch = 15, col = pal, levels(colCategories),
+               bty = "n", cex = 2)
       }
-      segments(xy[parent, 1], xy[parent, 2],
-               xy[child, 1], xy[child, 2],
-               col = "#00000033")
-      points(xy[parent, 1], xy[parent, 2], pch = ".", col = "#00000099")
+      if (!input$pch %in% c("Fixed", "Cluster")) {
+        legend("left", col = 1, cex = 2, bty = "n",
+               pch = seq_along(levels(pchCategories)),
+               levels(pchCategories))
+      }
+      
+      # poEdge <- Postorder(tree())$edge
+      # parent <- poEdge[, 1]
+      # child <- poEdge[, 2]
+      # xy <- matrix(0, parent[1], 2)
+      # xy[seq_len(NTip(tree())), ] <- mapping()
+      # for (node in unique(parent)) {
+      #   xy[node, ] <- colMeans(xy[child[parent == node], , drop = FALSE])
+      # }
+      # segments(xy[parent, 1], xy[parent, 2],
+      #          xy[child, 1], xy[child, 2],
+      #          col = "#00000033")
+      # points(xy[parent, 1], xy[parent, 2], pch = ".", col = "#00000099")
     }
   }
   
