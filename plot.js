a773d569f2fddb1b42dc5bbfcf9cc99a547ff136
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2022-06-02 12:49:26 +0100

    Allow text

--- plot.js
+++ plot.js
@@ -1,4 +1,4 @@
-// !preview r2d3 data = cbind(d0 = c(0, 0, 3, 8), d1 = c(0, 0, 3, 8), d2 = c(3, 3, 0, 5), d3 = c(8, 8, 5, 0), mappedX = c(1, 1, 0, 0), mappedY = c(1, 0, 0, 1), read.csv("Testing_Nextstrain_Metadata.csv", row.names = 1)[1:4, ]), viewer = "internal", options = list(col = hcl.colors(4), txt = letters[1:4])
+// !preview r2d3 data = cbind(d0 = c(0, 0, 3, 8), d1 = c(0, 0, 3, 8), d2 = c(3, 3, 0, 5), d3 = c(8, 8, 5, 0), mappedX = c(1, 1, 0, 0), mappedY = c(1, 0, 0, 1), read.csv("Testing_Nextstrain_Metadata.csv", row.names = 1)[1:4, ]), options = list(col = hcl.colors(4), txt = letters[1:4]), viewer = "internal"
 
 var links = function() {
       var ret = [];
@@ -15,20 +15,32 @@ function radius (d) {
 }
 
 function ticked() {
-  const u = svg.selectAll("circle")
-    .data(data)
-    .join("circle")
-    .attr("r", radius)
-    .attr("cx", function(d) {
-      return d.x
-    })
-    .attr("cy", function(d) {
-      return d.y
-    })
-    .attr("fill", function (d, i) {
-      return options.col[i];
-    })
-    ;
+  var u = svg
+      .selectAll(".node-group")
+      .data(data)
+      .join(enter => {
+        var node = enter
+            .append("g")
+            .attr("class", "node-group")
+            ;
+        
+        node.append("circle")
+            .attr("r", radius)
+            .attr("cx", 0)
+            .attr("cy", 0)
+            .attr("fill", function (d, i) {
+              return options.col[i];
+            });
+          
+        node.append("text")
+            .text(function(d, i) {return options.txt[i];})
+            .attr("font-family", "\"Gill Sans\", \"Gill Sans MT\", Arial")
+            .attr("text-anchor", "middle")
+            .attr("dominant-baseline", "central");
+      })
+      .attr("transform", function(d) {
+              return "translate(" + d.x + "," + d.y + ")";
+            });
 }
 
 var simulation = d3.forceSimulation(data)
@@ -47,5 +59,7 @@ var simulation = d3.forceSimulation(data)
     .strength(0.9)
   )
   .force("collision", d3.forceCollide().radius(radius).strength(1))
-  .on('tick', ticked);
+  .on("tick", ticked);
+  
+  // simulation.find(x, y) returns nearest node
   
